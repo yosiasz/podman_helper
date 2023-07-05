fe3620a8e7755c6027b6bacd216ed6318533671e
ዮስያስ <yosiasz@gmail.com>
2023-07-05 14:06:46 -0700

    additional docker compose stacks

--- influxdb/import_data.py
+++ influxdb/import_data.py
@@ -19,16 +19,23 @@ Write data into InfluxDB
 client = InfluxDBClient(url=URL, token=TOKEN, org=ORG, debug=True)
 write_api = client.write_api(write_options=SYNCHRONOUS)  
 
-col_list = ["_time","_field","_value"]
-df = pd.read_csv("./data/geomap.csv", usecols=col_list)
+col_list = ["_time","_value","DeviceID","_field","_measurement","host","topic"]
+df = pd.read_csv("./data/wakanda.csv", usecols=col_list)
 for index, row in df.iterrows():
     _time =  row['_time']
     _field =  row['_field']
-    _value =  float(row['_value'])
-  
-    point = Point("device_location") \
-                .tag("type", "speed") \
+    _value =  row['_value']
+    DeviceID = row['DeviceID']
+    _measurement = row['_measurement']
+    host = row['host']
+    topic = row['topic']
+    
+    point = Point(_measurement) \
+                .tag("tag1", "statuses") \
                 .field(_field, _value) \
+                .field("DeviceID", DeviceID) \
+                .field("host", host) \
+                .field("topic", topic) \
                 .time(_time, WritePrecision.NS)
 
     print(point)
