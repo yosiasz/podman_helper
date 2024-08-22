895c506765b6cd368e853742f0e9a8e39c2b8e74
ዮስያስ <yosiasz@gmail.com>
2024-08-22 15:37:33 -0700

    elastic,allow,observium

--- mongodb/populate.py
+++ mongodb/populate.py
@@ -1,4 +1,4 @@
-import prestodb
+""" import prestodb
 conn=prestodb.dbapi.connect(
     host='localhost',
     port=8080,
@@ -10,4 +10,22 @@ conn=prestodb.dbapi.connect(
 )
 cur = conn.cursor()
 cur.execute('select *, now() from tpch.sf1.orders limit 10')
-rows = cur.fetchall()
+rows = cur.fetchall() """
+
+import pymongo
+import json
+
+url = "mongodb://root:example@mongo:27017/"
+client = pymongo.MongoClient("mongodb://root:example@127.0.0.1:27017/")
+db = client["grafana"]
+col = db["customers"]
+
+mydata = { "name": "John", "address": "Highway 37" }
+
+fp = open('customers.json')
+
+data = json.load(fp)
+
+print(data)
+
+x = col.insert_one(data)
