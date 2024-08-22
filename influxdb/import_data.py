d287cf3164d16fa8067275c722e31bd677e64321
ዮስያስ <yosiasz@gmail.com>
2024-08-22 15:37:33 -0700

    elastic,allow,observium

--- influxdb/import_data.py
+++ influxdb/import_data.py
@@ -6,12 +6,44 @@ from dotenv import load_dotenv
 
 import os
 sure=load_dotenv()
-TOKEN = ''
+TOKEN = 'dS82CctBqX4N-sxwVD7r4j2Nv0vSu0mBINgKLHgU-5XQqltXuYwzZxkbrOJaHgouaSmuXYloPT1KX-MVtDZj7A=='
 #os.getenv('INFLUX_TOKEN')
-URL = os.getenv('INFLUX_URL')
-ORG = os.getenv('INFLUX_ORG')
+URL = 'localhost:8086'
+#os.getenv('INFLUX_URL')
+ORG = 'research'
+#os.getenv('INFLUX_ORG')
 
+#,
+def load_bie():
+    bucket = "bie"
 
+    """
+    Write data into InfluxDB
+    """
+    client = InfluxDBClient(url=URL, token=TOKEN, org=ORG, debug=True)
+    write_api = client.write_api(write_options=SYNCHRONOUS)  
+    #result,table,_time,_value,baseline
+    #0,2024-04-11T08:28:42.073Z,3339,cspcd.cis_master.10189
+    col_list = ["table","_time","_value","baseline"]
+    df = pd.read_csv("./data/bobo.csv", usecols=col_list)
+    for index, row in df.iterrows():
+        _time =  row['_time']
+        _field =  row['baseline']
+        _value =  float(row['_value'])
+        
+        point = Point('bie_bie') \
+                    .tag("tag1", "bie") \
+                    .field(_field, _value) \
+                    .time(_time, WritePrecision.NS)
+
+        print(point)
+        write_api.write(bucket, ORG, point) 
+    write_api.__del__()
+
+    """
+    Close client
+    """
+    client.__del__()
 def load_speed():
     #print(datetime.utcnow().timestamp()*10**9)
     bucket = "gpstracking_db"
@@ -113,8 +145,9 @@ def load_parking_lat():
     """
     client.__del__()    
 def main():
-    load_parking_lat()
-    load_parking_lon()
+    #load_parking_lat()
+    #load_parking_lon()
+    load_bie()
 
 if __name__ == '__main__':
     main()
