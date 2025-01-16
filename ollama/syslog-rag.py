99a96796539ffa4842b0a3466c710b5ee1be7e1b
ዮስያስ <yosiasz@gmail.com>
2025-01-16 09:23:36 -0800

    added some alloy ingests, ollama training etc

---
+++ ollama/syslog-rag.py
@@ -0,0 +1,128 @@
+import os
+import ollama
+import chromadb
+from chromadb.utils import embedding_functions
+#from langchain_community.vectorstores import Chroma
+
+
+DOC_PATH = "./data/sample.log"
+MODEL_NAME = "llama3.2"
+EMBEDDING_MODEL = "nomic-embed-text"
+VECTOR_STORE_NAME = "simple-rag"
+
+# Initialize the Chroma client with persistence
+chroma_client = chromadb.PersistentClient(path="chroma_persistent_storage")
+collection_name = "document_qa_collection"
+collection = chroma_client.get_or_create_collection(
+    name=collection_name, embedding_function=EMBEDDING_MODEL
+)
+
+# Function to load documents from a directory
+def load_documents_from_directory(directory_path):
+    print("==== Loading documents from directory ====")
+    documents = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".log"):
+            with open(
+                os.path.join(directory_path, filename), "r", encoding="utf-8"
+            ) as file:
+                documents.append({"id": filename, "text": file.read()})
+    return documents
+
+def humanize_syslog():
+    reg = r'(<\d{3}>\d{1}) (\d{4}\-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}\+\d{2}:\d{2}) (\w*)'
+    
+# Function to split text into chunks
+def split_text(text, chunk_size=1000, chunk_overlap=20):
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start = end - chunk_overlap
+    return chunks
+
+
+# Load documents from the directory
+directory_path = "./data/syslogs"
+documents = load_documents_from_directory(directory_path)
+
+print(f"Loaded {len(documents)} documents")
+# Split documents into chunks
+chunked_documents = []
+for doc in documents:
+    chunks = split_text(doc["text"])
+    print("==== Splitting docs into chunks ====")
+    for i, chunk in enumerate(chunks):
+        chunked_documents.append({"id": f"{doc['id']}_chunk{i+1}", "text": chunk})
+
+# print(f"Split documents into {len(chunked_documents)} chunks")
+# Generate embeddings for the document chunks
+for doc in chunked_documents:
+    print("==== Generating embeddings... ====")
+    print(doc)
+    #doc["embedding"] = 
+    #get_openai_embedding(doc["text"])
+
+# print(doc["embedding"])
+
+# Upsert documents with embeddings into Chroma
+for doc in chunked_documents:
+    print("==== Inserting chunks into db;;; ====")
+    collection.upsert(
+        ids=[doc["id"]], documents=[doc["text"]], embeddings=[doc["embedding"]]
+    )
+
+
+# Function to query documents
+def query_documents(question, n_results=2):
+    # query_embedding = get_openai_embedding(question)
+    results = collection.query(query_texts=question, n_results=n_results)
+
+    # Extract the relevant chunks
+    relevant_chunks = [doc for sublist in results["documents"] for doc in sublist]
+    print("==== Returning relevant chunks ====")
+    return relevant_chunks
+    # for idx, document in enumerate(results["documents"][0]):
+    #     doc_id = results["ids"][0][idx]
+    #     distance = results["distances"][0][idx]
+    #     print(f"Found document chunk: {document} (ID: {doc_id}, Distance: {distance})")
+
+
+# Function to generate a response from OpenAI
+def generate_response(question, relevant_chunks):
+    context = "\n\n".join(relevant_chunks)
+    prompt = (
+        "You are an assistant for question-answering tasks. Use the following pieces of "
+        "retrieved context to answer the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the answer concise."
+        "\n\nContext:\n" + context + "\n\nQuestion:\n" + question
+    )
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": prompt,
+            },
+            {
+                "role": "user",
+                "content": question,
+            },
+        ],
+    )
+
+    answer = response.choices[0].message
+    return answer
+
+
+# Example query
+# query_documents("tell me about AI replacing TV writers strike.")
+# Example query and response generation
+question = "tell me about databricks"
+relevant_chunks = query_documents(question)
+answer = 'hello'
+#generate_response(question, relevant_chunks)
+
+print(answer)
---
+++ ollama/syslog-to-human-speech.py
@@ -0,0 +1,5 @@
+import os
+
+with open('./data/sample.log') as file:
+    for line in file:
+        print(line)
